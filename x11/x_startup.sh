0dd446e46591f45a2af4384fca7c0ecf78d58367
anon <anon@anon.anon>
2025-02-09 17:43:12 +0100

    upped by keyboard sensibility

--- x11/x_startup.sh
+++ x11/x_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 xset s off
-xset r rate 150 50
+xset r rate 140 55
 setxkbmap -layout hu,ru -variant nodeadkeys,phonetic -option grp:alt_caps_toggle
 setxkbmap -option caps:swapescape
