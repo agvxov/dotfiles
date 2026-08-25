7f9ff3a9cedf122b9db12a3a08c2c49b55ea8392
anon <anon@anon.anon>
2026-08-25 09:41:16 +0200

    Comment confusing xset command notation

--- x11/x_startup.sh
+++ x11/x_startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 xset s off
+
+# i milliseconds delay; h repeats per second
 xset r rate 140 55
+
 setxkbmap -layout hu,ru -variant nodeadkeys,phonetic -option grp:alt_caps_toggle
 setxkbmap -option caps:swapescape
